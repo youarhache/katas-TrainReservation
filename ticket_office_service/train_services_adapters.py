9da13cada94fd41591aaa6147e40d1da74e20bc2
youarhache <youarhache@gmail.com>
2022-01-14 16:39:02 +0100

    Adding a dataclass Seat and refactoring

--- ticket_office_service/train_services_adapters.py
+++ ticket_office_service/train_services_adapters.py
@@ -1,24 +1,39 @@
-import urllib.request
+from dataclasses import dataclass
+import requests
 import json
+
+
+@dataclass
+class Seat:
+    seat_name: str
+    seat_number: str
+    coach: str
+    booking_reference: str
 
 
 class TrainDataAdapter:
     URL = "http://127.0.0.1:8081"
     
     def get_train_data(self, train_id):
-        train_data = urllib.request.urlopen(self.URL + f"/data_for_train/{train_id}")
-        return json.loads(train_data.read().decode("utf-8"))
+        response = requests.get(self.URL + f"/data_for_train/{train_id}")
+        train_data = json.loads(response)
+        if "seats" not in train_data:
+            return None
+        seats = []
+        for seat_name, seat_data in train_data["seats"].items():
+            seats.append(Seat(seat_name, seat_data.get("seat_number"), seat_data.get("coach"), seat_data.get("booking_reference")))
+            
+        return seats
 
     def reserve(self, train_id, seats, booking_reference):
         form_data = {"train_id": train_id, "seats": json.dumps(seats), "booking_reference": booking_reference}
-        data = urllib.parse.urlencode(form_data)
-        req = urllib.request.Request(self.URL + "/reserve", bytes(data, encoding="ISO-8859-1"))
-        return f"situation after reservation: {urllib.request.urlopen(req).read().decode('utf-8')}"
+        req = requests.post(self.URL + "/reserve", data=form_data)
+        return f"situation after reservation: {req}"
 
 
 class BookingReferenceAdapter:
     URL = "http://127.0.0.1:8082"
 
     def get_booking_reference(self):
-        data = urllib.request.urlopen(self.URL + "/booking_reference")
-        return data.read().decode("utf-8")
+        data = requests.get(self.URL + "/booking_reference")
+        return data
